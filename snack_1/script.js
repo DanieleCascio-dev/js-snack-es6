a738491054d1128619b01ffee8780ddf5b86da35
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-11-09 09:38:17 +0100

    fix snack_1

--- snack_1/script.js
+++ snack_1/script.js
@@ -19,9 +19,8 @@ const bicycles = [
 
 /* const [bike1, bike2, bike3] = bicycles; */
 const theLighter = isLess(bicycles[0], bicycles[1], bicycles[2]);
-console.log(
-  `La bici con peso minore è ${theLighter.name} e pesa ${theLighter.weight}`
-);
+const { name, weight } = theLighter;
+console.log(`La bici con peso minore è ${name} e pesa ${weight}`);
 /* bicycles.forEach((bike) => {
   const { weight } = bike;
   console.log(weight);
